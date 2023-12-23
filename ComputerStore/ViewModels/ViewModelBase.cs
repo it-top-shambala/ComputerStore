1afc34b472c7ed0b8b6d139599685e6fe42a56dc
Starinin Andrey <starininandrey@gmail.com>
2023-12-23 14:14:12 +0300

    Init Avalonia project

---
+++ ComputerStore/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace ComputerStore.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ ComputerStore/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ComputerStore.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
